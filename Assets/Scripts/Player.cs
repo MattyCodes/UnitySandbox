286fc3b3ca8f4f82415a6ce171f76bddc13ed22f
MattyCodes <matt.nick.fagioli@gmail.com>
2025-04-06 17:10:28 -0400

    Refactor movement and jumping to use the character controller component.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,100 +1,85 @@
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
-    [SerializeField] private float movementSpeed = 5f;
+public class Player : MonoBehaviour {
+    [SerializeField] private float jumpHeight = 3.5f;
+    [SerializeField] private float gravity = -20f;
+    [SerializeField] private float movementSpeed = 10f;
     [SerializeField] private float rotationSpeed = 15f;
-    [SerializeField] private float playerRadius = 0.7f;
-    [SerializeField] private float playerHeight = 2f;
+    // [SerializeField] private float playerRadius = 0.7f;
+    // [SerializeField] private float playerHeight = 2f;
     [SerializeField] private GameInput gameInput;
 
-    private bool isWalking;
-    private bool isJumping;
+    private CharacterController controller;
+    private Vector3 velocity;
 
-    private void Update()
-    {
-        // On every frame, check for jump-input from the player.
-        isJumping = gameInput.GetJumpingValue();
+    private bool isWalking = false;
+    private bool isJumping = false;
+    private bool isGrounded = true;
 
-        // Also get any movement inputs (WASD) from the player.
+    public bool IsWalking() => isWalking;
+    public bool IsJumping() => isJumping;
+
+    private void Awake() {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update() {
+        HandleMovement();
+    }
+
+    private void HandleMovement() {
+        // Manage the state of player velocity based on whether
+        // or not they are grounded/jumping.
+        isGrounded = controller.isGrounded;
+
+        // If they're grounded and not jumping, just set a baseline
+        // downward force to keep 'em that way.
+        if (isGrounded && velocity.y < 0) {
+            velocity.y = -2f;
+            isJumping = false;
+        }
+
+        // If they are grounded and we've received a jump button-press
+        // during this frame, the player is now jumping and their
+        // upward velocity should be updated accordingly.
+        if (gameInput.GetJumpingValue() && isGrounded) {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            isJumping = true;
+        }
+
+        // As a general rule, if the player is in the air, then their downward
+        // velocity should increase according to gravity until they are grounded.
+        if (!isGrounded) {
+            velocity.y += gravity * Time.deltaTime;
+        }
+
+        // Gather the directional input (WASD, or equivalent) from the user, and
+        // derive a 3D vector from it.
         Vector2 inputVector = gameInput.GetMovementVector().normalized;
+        Vector3 inputDirection = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        // Get the intended movement direction.
-        Vector3 radialMoveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
-
-        // Get the X vector of the intended movement direction.
-        Vector3 xMoveDirection = new Vector3(radialMoveDirection.x, 0f, 0f);
-
-        // Get the Z vector of the intended movement direction.
-        Vector3 zMoveDirection = new Vector3(0f, 0f, radialMoveDirection.z);
-
-        // Determine the traversable distance if no collisions occur.
-        float unhinderedMoveDistance = movementSpeed * Time.deltaTime;
-
-        // If collisions do occur, but the player can still move in the
-        // direction of one of the specified vectors, reduce the traversable
-        // distance by 25%.
-        float hinderedMoveDistance = unhinderedMoveDistance * 0.75f;
-
-        // Determine if the user is capable of moving in the exact
-        // direction that was specified without colliding with anything.
-        bool canMoveRadial = !Physics.CapsuleCast(
-            transform.position,
-            transform.position + Vector3.up * playerHeight,
-            playerRadius,
-            new Vector3(inputVector.x, 0f, inputVector.y),
-            unhinderedMoveDistance
-        );
-
-        // For the three possible input vectors, determine if it is possible
-        // to move without a collision occurring.
-        if (canMoveRadial) {
-            transform.position += radialMoveDirection * unhinderedMoveDistance;
-            isWalking = radialMoveDirection != Vector3.zero;
-        } else {
-            bool canMoveX = !Physics.CapsuleCast(
-                transform.position,
-                transform.position + Vector3.up * playerHeight,
-                playerRadius,
-                xMoveDirection,
-                hinderedMoveDistance
-            );
-
-            if (canMoveX) {
-                transform.position += xMoveDirection * hinderedMoveDistance;
-                isWalking = xMoveDirection != Vector3.zero;
-            } else {
-                bool canMoveZ = !Physics.CapsuleCast(
-                    transform.position,
-                    transform.position + Vector3.up * playerHeight,
-                    playerRadius,
-                    zMoveDirection,
-                    hinderedMoveDistance
-                );
-
-                if (canMoveZ) {
-                    transform.position += zMoveDirection * hinderedMoveDistance;
-                    isWalking = zMoveDirection != Vector3.zero;
-                } else {
-                    isWalking = false;
-                };
-            };
-        };
-
-        // Even if the player cannot move in the desired
-        // direction, allow them to rotate in place.
+        // Rotate the player in the direction they are trying to
+        // move, even if a collision prevents them from moving there.
         transform.forward = Vector3.Slerp(
             transform.forward,
-            radialMoveDirection,
+            inputDirection,
             rotationSpeed * Time.deltaTime
         );
-    }
 
-    public bool IsWalking() {
-        return isWalking;
-    }
+        // The player should only be considered "walking" if the derived
+        // input-vector is non-zero in at least one direction.
+        if (inputDirection != Vector3.zero) {
+            isWalking = true;
+        } else {
+            isWalking = false;
+        };
 
-    public bool IsJumping() {
-        return isJumping;
+        // Instantiate the movement vector as a result of the input
+        // direction, and then apply upward/downward velocity to it.
+        Vector3 playerMovement = inputDirection * movementSpeed;
+        playerMovement.y = velocity.y;
+
+        // Affect the character controller with the desired player movement.
+        controller.Move(playerMovement * Time.deltaTime);
     }
 }
